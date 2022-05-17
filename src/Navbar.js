ee9dc452e605753b0cc694b7aeb973eb73e38cc6
Jay Kalia <1895567@tcs.com>
2022-05-17 21:32:38 +0530

    minor changes

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ export default function Navbar(){
     return(
         <div className="flex" >
             <div className="w-2/3" >
-                <h3 className=" text-4xl font-bold p-2 " >Dark Mode in React</h3>
+                <h3 className=" text-4xl font-bold p-2 text-gray-500 dark:text-gray-400" >Dark Mode in React</h3>
             </div>
             <div className=" w-1/3 " >
                     <div className="text-right ml-96" >
